eb089b7c704881ede76700f76dbc0c10e4514897
Siddhant Gupta <guptasiddhant@icloud.com>
2024-05-26 18:12:53 +0300

    Fix github action

--- scripts/bump.mjs
+++ scripts/bump.mjs
@@ -4,8 +4,10 @@ import { readFileSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
 
 const strategy = process.argv[2] || "patch";
+/** @type {import("node:child_process").ExecSyncOptions} */
+const options = { encoding: "utf-8", stdio: "inherit" };
 
-execSync(`yarn version ${strategy}`);
+execSync(`yarn version ${strategy}`, options);
 
 const cwd = process.cwd();
 const pkgJsonPath = join(cwd, "package.json");
@@ -20,14 +22,14 @@ console.log("Updating JSR version to ", version);
 jsrJson.version = version;
 writeFileSync(jsrJsonPath, JSON.stringify(jsrJson, null, 2));
 
-execSync(`git add package.json jsr.json`);
-execSync(`git commit -m 'bump to v${version}'`);
+execSync(`git add package.json jsr.json`, options);
+execSync(`git commit -m 'bump to v${version}'`, options);
 
 console.log("Creating tag v" + version);
-execSync(`git tag -a v${version}`);
+execSync(`git tag -a 'v${version}' -m "Bump to v${version}"`, options);
 
 console.log("Pushing to origin");
-execSync(`git push --tags`);
-execSync(`git push`);
+execSync(`git push --tags`, options);
+execSync(`git push`, options);
 
 process.exit(0);
